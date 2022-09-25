75b6da153b26fe215adc1b0d5811d301c08bd0d7
Ludson <ludson_ps27@hotmail.com>
2022-09-25 19:04:35 -0300

    Requisitos 57, 58, 59, 60, 61 e 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import imageProfile from '../images/profileIcon.svg';
 
-function Profile() {
+function Profile({ history }) {
+  const [email, setEmail] = useState('');
+
+  const getEmailLocalStorage = () => {
+    const newEmail = localStorage.getItem('user');
+    setEmail(JSON.parse(newEmail));
+  };
+
+  useEffect(() => {
+    getEmailLocalStorage();
+  }, []);
+
+  const clickLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
-      <h1 data-testid="page-title">Profile</h1>
-      <Link to="/profile">
-        <img src={ imageProfile } alt="imagem de perfil" data-testid="profile-top-btn" />
-      </Link>
+      <header>
+        <Link to="/profile">
+          <img
+            src={ imageProfile }
+            alt="imagem de perfil"
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <h1 data-testid="page-title">Profile</h1>
+      </header>
+
+      <main>
+        <p data-testid="profile-email">
+          { email.email }
+        </p>
+        <div>
+
+          <Link to="/done-recipes" data-testid="profile-done-btn">
+            Done Recipes
+          </Link>
+
+          <Link to="/favorite-recipes" data-testid="profile-favorite-btn">
+            Favorite Recipes
+          </Link>
+
+          <Link
+            to="/"
+            data-testid="profile-logout-btn"
+            onClick={ clickLogout }
+          >
+            Logout
+          </Link>
+
+        </div>
+      </main>
+
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Profile;
