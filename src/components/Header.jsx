0827d44a92de5db6384a568d6ad9de45de7bc59c
Ivens <ivensarroxellas@hotmail.com>
2022-09-26 15:25:33 -0300

    req 9

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import imageProfile from '../images/profileIcon.svg';
 import imageSearch from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function Header() {
+const [renderSearchBar, setSearchBar] = useState(false);
+
+const showSearchBar = () => {
+  if(renderSearchBar){
+    setSearchBar(false)
+  } else { setSearchBar(true)}
+}
   return (
     <header>
       <Link to="/profile">
         <img src={ imageProfile } alt="imagem de perfil" data-testid="profile-top-btn" />
       </Link>
-      <img src={ imageSearch } alt="imagem de pesquisa" data-testid="search-top-btn" />
+      <button
+        data-testid="search-artist-button"
+        type="button"
+        onClick={ showSearchBar }
+        >
+          <img src={ imageSearch } alt="" />
+        </button>
+        {renderSearchBar && <SearchBar />}
     </header>
   );
 }
