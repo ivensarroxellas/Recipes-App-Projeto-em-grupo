c216e440ffc7899f421fab472dc4441ca6b3e9ba
Ludson <ludson_ps27@hotmail.com>
2022-10-04 10:50:09 -0300

    Requisito 55

--- src/pages/FavoritedRecipes.jsx
+++ src/pages/FavoritedRecipes.jsx
@@ -9,6 +9,7 @@ import blackHeartIcon from '../images/blackHeartIcon.svg';
 function FavoritedRecipes() {
   const [shareCopyRender, setShareCopyRender] = useState(false);
   const [getRecipeStorage, setGetRecipeStorage] = useState([]);
+  const [showRecipe, setShowRecipe] = useState([]);
   const [favIcon, setFavIcon] = useState(true);
 
   const loadLocalStorage = () => {
@@ -16,10 +17,13 @@ function FavoritedRecipes() {
     if (getLocalStorage) {
       setGetRecipeStorage(getLocalStorage);
     }
+    setShowRecipe(getLocalStorage);
   };
 
   useEffect(() => {
     loadLocalStorage();
+    console.log('showRecipe', showRecipe);
+    console.log('getRecipeStorage', getRecipeStorage);
     // eslint-disable-next-line
   }, []);
 
@@ -38,6 +42,20 @@ function FavoritedRecipes() {
     setShareCopyRender(true);
   }
 
+  const handleFilterMeals = () => {
+    const filterRecipe = getRecipeStorage.filter(({ type }) => type === 'meal');
+    setShowRecipe(filterRecipe);
+  };
+
+  const handleFilterDrinks = () => {
+    const filterRecipe = getRecipeStorage.filter(({ type }) => type === 'drink');
+    setShowRecipe(filterRecipe);
+  };
+
+  const handleFilterAll = () => {
+    setShowRecipe(getRecipeStorage);
+  };
+
   return (
     <>
       <header>
@@ -54,6 +72,7 @@ function FavoritedRecipes() {
         <button
           type="button"
           data-testid="filter-by-all-btn"
+          onClick={ handleFilterAll }
         >
           All
         </button>
@@ -61,6 +80,7 @@ function FavoritedRecipes() {
         <button
           type="button"
           data-testid="filter-by-meal-btn"
+          onClick={ handleFilterMeals }
         >
           Meals
         </button>
@@ -68,82 +88,83 @@ function FavoritedRecipes() {
         <button
           type="button"
           data-testid="filter-by-drink-btn"
+          onClick={ handleFilterDrinks }
         >
           Drinks
         </button>
 
-        {getRecipeStorage.map((recipe, index) => {
-          const { image, name, category, id, nationality, type, alcoholicOrNot } = recipe;
-          return (
-            <div key={ index }>
-              <img
-                src={ image }
-                alt={ name }
-                data-testid={ `${index}-horizontal-image` }
-              />
-              <h3 data-testid={ `${index}-horizontal-name` }>
-                {name}
-              </h3>
-              {
-                type === 'meal'
-                  ? (
-                    <h5 data-testid={ `${index}-horizontal-top-text` }>
-                      {`${nationality} - ${category}`}
-                    </h5>
-                  )
-                  : (
-                    <h5 data-testid={ `${index}-horizontal-top-text` }>
-                      {category}
-                    </h5>
-                  )
-              }
-              {
-                type === 'drink' && (
+        {showRecipe && showRecipe.map((
+          { image, name, category, id, nationality, type, alcoholicOrNot },
+          index,
+        ) => (
+          <div key={ index }>
+            <img
+              src={ image }
+              alt={ name }
+              data-testid={ `${index}-horizontal-image` }
+            />
+            <h3 data-testid={ `${index}-horizontal-name` }>
+              {name}
+            </h3>
+            {
+              type === 'meal'
+                ? (
                   <h5 data-testid={ `${index}-horizontal-top-text` }>
-                    {alcoholicOrNot}
+                    {`${nationality} - ${category}`}
                   </h5>
                 )
-              }
-              <h6 data-testid={ `${index}-horizontal-done-date` }>
-                {new Date().toLocaleDateString()}
-              </h6>
-              <div>
-                <button
-                  type="button"
-                  onClick={ () => handleClickShareBtn(type, id) }
-                >
-                  <img
-                    data-testid={ `${index}-horizontal-share-btn` }
-                    src={ shareIcon }
-                    alt="share"
-                  />
-                </button>
-                {shareCopyRender && <h4>Link copied!</h4>}
-                <button type="button" onClick={ () => handleFavorite(id) }>
-                  { favIcon
-                    ? (
-                      <img
-                        data-testid={ `${index}-horizontal-favorite-btn` }
-                        src={ blackHeartIcon }
-                        alt="favorite"
-                      />
-                    )
-                    : (
-                      <img
-                        data-testid={ `${index}-horizontal-favorite-btn` }
-                        src={ whiteHeartIcon }
-                        alt="not favorite"
-                      />
-                    )}
-                </button>
-              </div>
-
-              <p data-testid={ `${index}-${id}-horizontal-tag` }>
-                Tags
-              </p>
+                : (
+                  <h5 data-testid={ `${index}-horizontal-top-text` }>
+                    {category}
+                  </h5>
+                )
+            }
+            {
+              type === 'drink' && (
+                <h5 data-testid={ `${index}-horizontal-top-text` }>
+                  {alcoholicOrNot}
+                </h5>
+              )
+            }
+            <h6 data-testid={ `${index}-horizontal-done-date` }>
+              {new Date().toLocaleDateString()}
+            </h6>
+            <div>
+              <button
+                type="button"
+                onClick={ () => handleClickShareBtn(type, id) }
+              >
+                <img
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  src={ shareIcon }
+                  alt="share"
+                />
+              </button>
+              {shareCopyRender && <h4>Link copied!</h4>}
+              <button type="button" onClick={ () => handleFavorite(id) }>
+                { favIcon
+                  ? (
+                    <img
+                      data-testid={ `${index}-horizontal-favorite-btn` }
+                      src={ blackHeartIcon }
+                      alt="favorite"
+                    />
+                  )
+                  : (
+                    <img
+                      data-testid={ `${index}-horizontal-favorite-btn` }
+                      src={ whiteHeartIcon }
+                      alt="not favorite"
+                    />
+                  )}
+              </button>
             </div>
-          );
-        })}
+
+            <p data-testid={ `${index}-${id}-horizontal-tag` }>
+              Tags
+            </p>
+          </div>
+        ))}
       </main>
     </>
   );
